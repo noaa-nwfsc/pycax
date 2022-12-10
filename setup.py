3c53d47f31d2798a0589e373b7f9caed672c30ab
Eli Holmes <eli.holmes@noaa.gov>
2022-12-09 16:59:58 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup
 setup(
     # The package metadata is specified in setup.cfg but GitHub's downstream dependency graph
     # does not work unless we put the name this here too.
-    name="pyobis",
+    name="pyCAX",
     use_scm_version={
-        "write_to": "pyobis/_version.py",
+        "write_to": "pyCAX/_version.py",
         "write_to_template": '__version__ = "{version}"',
         "tag_regex": r"^(?P<prefix>v)?(?P<version>[^\+]+)(?P<suffix>.*)?$",
     },
