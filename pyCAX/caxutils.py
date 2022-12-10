e355b8081b690573e287070470c1ed7b3ce0e53b
Eli Holmes <eli.holmes@noaa.gov>
2022-12-09 17:35:24 -0800

    first stab

--- pyCAX/caxutils.py
+++ pyCAX/caxutils.py
@@ -5,7 +5,7 @@ from urllib.parse import urlencode
 
 import requests
 
-obis_baseurl = "https://api.obis.org/v3/"
+cax_baseurl = "https://api.streamnet.org/api/v1"
 
 
 class NoResultException(Exception):
@@ -22,25 +22,25 @@ def build_api_url(url, args):
     """
     return url + "?" + urlencode({k: v for k, v in args.items() if v is not None})
 
+def make_ua():
+    return {
+        "user-agent": "python-requests/"
+        + requests.__version__
+        + ",pyCAX/"
+        + pyCAX.__version__
+    }
 
-def obis_GET(url, args, ctype, **kwargs):
+def cax_GET(url, args, ctype, **kwargs):
     """
     Handles technical details of sending GET request to the API
     """
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)\
-         Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.52",
-        "Accept-Encoding": "gzip, deflate, br",
-        "Host": "api.obis.org",
-        "Connection": "keep-alive",
-    }
-    out = requests.get(url, params=args, headers=headers, **kwargs)
+    out = requests.get(url, params=args, headers=make_ua(), **kwargs)
     out.raise_for_status()
     stopifnot(out.headers["content-type"], ctype)
     return out.json()
+    
 
-
-def obis_write_disk(url, path, ctype, **kwargs):
+def cax_write_disk(url, path, ctype, **kwargs):
     out = requests.get(url, stream=True, **kwargs)
     out.raise_for_status()
     with open(path, "wb") as f:
