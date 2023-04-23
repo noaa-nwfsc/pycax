70df9795664bbf793ed74dde5f638b39e6981f50
Eli Holmes <eli.holmes@noaa.gov>
2023-04-23 14:27:57 -0400

    needed to import Path from pathlib

--- pycax/__init__.py
+++ pycax/__init__.py
@@ -54,6 +54,8 @@ __all__ = [
 ]
 
 # Set the bibtex entry to the misc entry referenced in CITATION.
+import sys
+from pathlib import Path
 def _get_bibtex():
     refs = (Path(__file__).parent / "CITATION").read_text().split("@misc")[1:]
     return f"@misc{refs[0]}" if refs else ""
